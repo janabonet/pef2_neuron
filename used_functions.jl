8b2cdd6e3728e2cf9a4ee341f952b42003f16a10
pol <polfloridt@gmail.com>
2023-04-19 19:24:09 +0200

    Creat la funcio per integrar el model

--- used_functions.jl
+++ used_functions.jl
@@ -1,1 +1,19 @@
 # Used functions and structs 
+
+module used_functions
+
+function hodgkin_huxley_deterministic!(du,u,p,t)
+
+    I_1 = @view u[1]
+    I_2 = @view u[2]
+    I_3 = @view u[3]
+
+    dI1 = @view du[1]
+    dI2 = @view du[2]
+    dI3 = @view du[3]
+
+     @. dI1 = funcio1
+     @. dI2 = funcio2
+
+    nothing
+end
