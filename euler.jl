cdcc2a53fe36ccc2cba2674392fc62e81a29a50f
janabonet <janaliubonet@gmail.com>
2023-04-25 00:54:26 +0200

    Simulació Hodgkin-Huxley determinista

--- euler.jl
+++ euler.jl
@@ -1,23 +1,3 @@
-# Used functions and structs 
-
-module used_functions #he afegit un comentari
-
-function hodgkin_huxley_deterministic!(du,u,p,t)
-
-    I_1 = @view u[1]
-    I_2 = @view u[2]
-    I_3 = @view u[3]
-
-    dI1 = @view du[1]
-    dI2 = @view du[2]
-    dI3 = @view du[3]
-
-     @. dI1 = funcio1
-     @. dI2 = funcio2
-
-    nothing
-end
-
 function euler(fun::function,t0 ,ci,step,m,D)
     using Random, Distributions
     #=
