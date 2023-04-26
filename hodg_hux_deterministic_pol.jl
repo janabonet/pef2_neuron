d60917e503866b7a5da3db153f89b8869be3fa82
pol <polfloridt@gmail.com>
2023-04-26 12:30:51 +0200

    Canviat funcio

--- hodg_hux_deterministic_pol.jl
+++ hodg_hux_deterministic_pol.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations, Plots, BenchmarkTools
+using DifferentialEquations, Plots, BenchmarkTools, StaticArrays
 
 # Potassium (n) and sodium (m,h) ion-channel rate functions
 
@@ -37,11 +37,27 @@ function hodg_hux_det!(du, u, p, t)
     @. dh = αh(V) * (1 - h) - βh(V)*h
 end
 
-# Callback to change external current
+function hodg_hux_det(u, p, t)
+    V_na, V_k, V_l, g_na, g_k, g_l, C, I_tot = p
+    # References to variables
+    V = u[1]
+    n = u[2]
+    m = u[3]
+    h = u[4]
 
-# condition(u,t,integrator) = t == 100
-# affect!(integrator) = integrator.p[8] += 1
-# cb = DiscreteCallback(condition,affect!)
+    # Channel currents
+    I_na =  g_na * m^3 * h * (V - V_na)
+    I_k  =  g_k * n^4 * (V - V_k)
+    I_l  =  g_l * (V- V_l)
+   
+    # ODE system
+     dV =  1/C * (I_tot -I_na - I_k - I_l)
+     dn =  αn(V) * (1 - n) - βn(V)*n
+     dm =  αm(V) * (1 - m) - βm(V)*m
+     dh =  αh(V) * (1 - h) - βh(V)*h
+    return [dV,dn,dm,dh]
+end
+# Callback to change external current
 
 current_step= PresetTimeCallback(100,integrator -> integrator.p[8] += 1)
 
@@ -58,18 +74,19 @@ I_tot = 0.0;
 p = Float64[V_na, V_k, V_l, g_na, g_k, g_l, C, I_tot];
 
 #Initial conditions
-#u₀ = [-60 ; 0.007 ; 0.083; 0.41]
-n_inf(v) = αn(v) / (αn(v) + βn(v))
-m_inf(v) = αm(v) / (αm(v) + βm(v))
-h_inf(v) = αh(v) / (αh(v) + βh(v))
-u₀ = [-60, n_inf(-60), m_inf(-60), h_inf(-60)]
+n_inf(v) = αn(v) / (αn(v) + βn(v));
+m_inf(v) = αm(v) / (αm(v) + βm(v));
+h_inf(v) = αh(v) / (αh(v) + βh(v));
+v₀ = -60;
+u₀ = SA[v₀, n_inf(v₀), m_inf(v₀), h_inf(v₀)]
 tspan = (0,1000);
 
 # Integration
-prob = ODEProblem(hodg_hux_det!,u₀, tspan, p,  dtmax = 0.01)
+prob = ODEProblem(hodg_hux_det,u₀, tspan, p,  dtmax = 0.01)
 
 sol = solve(prob, saveat = 0.1, callback = current_step)
 
+
 #figures
-plot(sol.t, sol[1,:], color = :blue ,title = "Time series of voltage", xlabel = "t (ms)", ylabel = "V (mV)", linewidth = 1)
+plot(sol.t, sol[1,:] ,title = "Time series of voltage", xlabel = "t (ms)", ylabel = "V (mV)", linewidth = 1)
 
