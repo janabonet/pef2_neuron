523ba274ef80f7bf116eba5c902e3a50fdba4b1e
pol <polfloridt@gmail.com>
2023-05-20 14:26:42 +0200

    Posat un N_tot que faltava, fet algun plot i passat vectors de gates a int64

--- gates_stochastic_euler_pol.jl
+++ gates_stochastic_euler_pol.jl
@@ -1,4 +1,4 @@
-using Plots, Distributions
+using Plots, Distributions, LoopVectorization
 
 # Parametres per Hodgkin Huxley
 const V_na = 55.0;
@@ -8,73 +8,79 @@ const g_na = 40.0;
 const g_k = 35.0;
 const g_l = 0.3;
 const C = 1.0;
-const I_ext = 0.0;
+I_ext = 0.0;
 p = [V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext];
 
 # Potassium (n) and sodium (m,h) ion-channel rate functions
-αₙ(V) = (0.02 * (V - 25.0)) / (1.0 - exp((-1.0 * (V - 25.0)) / 9.0));
-αₘ(V) = (0.182 * (V + 35.0)) / (1.0 - exp((-1.0 * (V + 35.0)) / 9.0));
-αₕ(V) = 0.25 * exp((-1.0 * (V + 90.0)) / 12.0);
-
 function α(V)
-    return [αₙ(V),αₘ(V),αₕ(V)]
+    return [
+        (0.02 * (V - 25.0)) / (1.0 - exp((-1.0 * (V - 25.0)) / 9.0)),
+        (0.182 * (V + 35.0)) / (1.0 - exp((-1.0 * (V + 35.0)) / 9.0)),
+        0.25 * exp((-1.0 * (V + 90.0)) / 12.0)
+    ]
 end
-
-βₙ(V) = (-0.002 * (V - 25.0)) / (1.0 - exp((V - 25.0) / 9.0));
-βₘ(V) = (-0.124 * (V + 35.0)) / (1.0 - exp((V + 35.0) / 9.0));
-βₕ(V) = (0.25 * exp((V + 62.0) / 6.0)) / exp((V + 90.0) / 12.0);
-
 function β(V)
-    return [βₙ(V), βₘ(V), βₕ(V) ]
+    return [
+        (-0.002 * (V - 25.0)) / (1.0 - exp((V - 25.0) / 9.0)),
+        (-0.124 * (V + 35.0)) / (1.0 - exp((V + 35.0) / 9.0)),
+        (0.25 * exp((V + 62.0) / 6.0)) / exp((V + 90.0) / 12.0)
+    ]
 end
 
-# Parametres simulacio
-N_tot = 100; # Nombre total de canals
-dt = 1e-1;   # time steps
-t_tot = 500;
-
-
+# Struct to have solution in a concise form
 struct solution
     t::Vector{Float64}
     V::Vector{Float64}
-    N_o::Matrix{Float64}
-    N_c::Matrix{Float64}
+    N_o::Matrix{Int64}
+    N_c::Matrix{Int64}
 end
 
+# Function to simulate 
 function hh_gates_simulation(N_tot, dt,t_tot, p)
     V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext = p
     total_steps = Int(t_tot/dt+1);
 
+    # Inicialitzar vectors
     V = zeros(total_steps)
-    N_o = zeros(3,total_steps)
-    N_c = zeros(3,total_steps)
+    N_o = zeros(Int64,3,total_steps)
+    N_c = zeros(Int64,3,total_steps)
 
+    # Condicions inicials
     V[1] = rand()
     N_o[:,1] = round.(N_tot*rand(3))
-    N_c[:,1] = N_tot .- N_o[:,1]
+    @. N_c[:,1] = N_tot - N_o[:,1]
 
+    # Evolucio temporal
     for i in 2:total_steps
         p₁ = α(V[i-1]).*N_o[:,i-1]
-        p₂ = β(V[i-1]).*N_c[:,i-1]; @show p₂
+        p₂ = β(V[i-1]).*N_c[:,i-1] 
 
-        a₁ = [rand(Poisson(p₁_i*dt)) for p₁_i in p₁]
-        a₂ = [rand(Poisson(p₂_i*dt)) for p₂_i in p₂]
+        a₁ = [rand(Poisson(p₁_i*dt)) for p₁_i in p₁];
+        a₂ = [rand(Poisson(p₂_i*dt)) for p₂_i in p₂];
 
         N_o[:,i] = N_o[i-1] .+ (a₂-a₁)
         N_c[:,i] = N_c[i-1] .+ (a₁-a₂)
 
-
-        I_na =  g_na * (N_o[2,i]/N_tot)^3 * N_o[3,i] * (V[i-1] - V_na)
-        I_k  =  g_k * (N_o[1,i]/N_tot).^4 * (V[i-1] - V_k)
+        # N_o = [n_oberts ; m_oberts; h_oberts]
+        I_na =  g_na * (N_o[2,i]/N_tot)^3 * N_o[3,i]/N_tot * (V[i-1] - V_na)
+        I_k  =  g_k * (N_o[1,i]/N_tot)^4 * (V[i-1] - V_k)
         I_l  =  g_l * (V[i-1] - V_l)
-
-        V[i] = V[i-1] + dt* 1/C*(I_ext - I_na - I_k - I_l)
     end
     return solution(collect(0:dt:t_tot),V,N_o,N_c)
 end
 
+# Parametres simulacio
+N_tot = 1000; # Nombre total de canals
+dt = 1e-3;   # time steps
+t_tot = 1000; # temps final
 
+# Simulacio
 sol = hh_gates_simulation(N_tot, dt,t_tot, p)
 
+# Gràfiques
+fig1 = plot(sol.t,sol.V, label = "V"; xlabel = "t", ylabel = "V")
+fig2 = plot(sol.t,(sol.N_o[1,:]/N_tot).^4, label = "n⁴", xlabel = "T")
+       plot!(sol.t,(sol.N_o[2,:]/N_tot).^3, label = "m³")
+       plot!(sol.t,sol.N_o[3,:]/N_tot, label = "h")
 
-
+plot(fig1,fig2, layout = (2,1))
