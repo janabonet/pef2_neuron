464f5bf52571a3266a2c8d7df4fcb3260116c3d9
pol <polfloridt@gmail.com>
2023-05-24 16:28:43 +0200

    fet codi per states stochastic. falta posar ifs per limits fisics

---
+++ channel_states_stochastic_pol.jl
@@ -0,0 +1,121 @@
+using Plots, Distributions
+# Parameters
+const V_na = 55.0;
+const V_k = -77.0;
+const V_l = -65.0;
+const g_na = 40.0;
+const g_k = 35.0;
+const g_l = 0.3;
+const C = 1.0;
+I_ext = 0.0;
+p = [V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext];
+# Gate functions
+αₙ(V) = (0.02 * (V - 25.0)) / (1.0 - exp((-1.0 * (V - 25.0)) / 9.0));
+αₘ(V) = (0.182 * (V + 35.0)) / (1.0 - exp((-1.0 * (V + 35.0)) / 9.0));
+αₕ(V) = 0.25 * exp((-1.0 * (V + 90.0)) / 12.0);
+
+βₙ(V) = (-0.002 * (V - 25.0)) / (1.0 - exp((V - 25.0) / 9.0));
+βₘ(V) = (-0.124 * (V + 35.0)) / (1.0 - exp((V + 35.0) / 9.0));
+βₕ(V) = (0.25 * exp((V + 62.0) / 6.0)) / exp((V + 90.0) / 12.0);
+
+
+struct solution
+    t::Vector{Float64}
+    V::Vector{Float64}
+    N4::Vector{Float64}
+    M3::Vector{Float64}
+    H::Vector{Float64}
+end
+
+function channel_states_euler(N_tot, dt, t_tot, p)
+    V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext = p
+
+    # integration Parameters
+    total_steps = Int(t_tot/dt+1);
+
+    # iniciar vectors
+    V = zeros(total_steps)
+    N0 = zeros(total_steps)
+    N1 = zeros(total_steps)
+    N2 = zeros(total_steps)
+    N3 = zeros(total_steps)
+    N4 = zeros(total_steps)
+    M0 = zeros(total_steps)
+    M1 = zeros(total_steps)
+    M2 = zeros(total_steps)
+    M3 = zeros(total_steps)
+    H  = zeros(total_steps)
+
+    # Initial conditions
+    V[1] = rand()
+    n0 = rand(5)*N_tot
+    m0 = rand(4)*N_tot
+    n0 = n0/sum(n0);
+    m0 = m0/sum(m0);
+    N0[1] = n0[1]
+    N1[1] = n0[2]
+    N2[1] = n0[3]
+    N3[1] = n0[4]
+    N4[1] = n0[5]
+    M0[1] = m0[1]
+    M1[1] = m0[2]
+    M2[1] = m0[3]
+    M3[1] = m0[4]
+    H[1] = rand()*1
+
+    for i in 2:Int(100/dt)
+        # Differential equation system
+        dN_n₀ = -4*αₙ(V[i-1])*N0[i-1] + βₙ(V[i-1])*N1[i-1]
+        dN_n₁ = -(3*αₙ(V[i-1]) + βₙ(V[i-1]))*N1[i-1] + 4*αₙ(V[i-1])*N0[i-1] + 2*βₙ(V[i-1])*N2[i-1]
+        dN_n₂ = -(2*αₙ(V[i-1]) + 2*βₙ(V[i-1]))*N2[i-1] + 3*αₙ(V[i-1])*N1[i-1] + 3*βₙ(V[i-1])*N3[i-1]
+        dN_n₃ = -(αₙ(V[i-1])+3*βₙ(V[i-1]))*N3[i-1] + 2*αₙ(V[i-1])*N2[i-1] + 4*βₙ(V[i-1])*N4[i-1]
+        dN_n₄ = -4*βₙ(V[i-1])*N4[i-1] + αₙ(V[i-1])*N3[i-1]
+        
+        dN_m₀ = -3*αₘ(V[i-1])*M0[i-1] + βₘ(V[i-1])*M1[i-1]
+        dN_m₁ = -(2*αₘ(V[i-1]) + βₘ(V[i-1]))*M1[i-1] + 3*αₘ(V[i-1])*M0[i-1] + 2*βₘ(V[i-1])*M2[i-1]
+        dN_m₂ = -(αₘ(V[i-1]) + 2*βₘ(V[i-1]))*M2[i-1] + 2*αₘ(V[i-1])*M1[i-1] + 3*βₘ(V[i-1])*M3[i-1]
+        dN_m₃ = -3*βₘ(V[i-1])*M3[i-1] + αₘ(V[i-1])*M2[i-1]
+
+        dN_h = αₕ(V[i-1])*(N_tot .- H[i-1]) - βₕ(V[i-1])*H[i-1]
+
+        # Evolucio canals 
+        N0[i] = N0[i-1] + rand(Poisson(dN_n₀*dt))
+        N1[i] = N1[i-1] + rand(Poisson(dN_n₁*dt))
+        N2[i] = N2[i-1] + rand(Poisson(dN_n₂*dt))
+        N3[i] = N3[i-1] + rand(Poisson(dN_n₃*dt))
+        N4[i] = N4[i-1] + rand(Poisson(dN_n₄*dt))
+        
+        M0[i] = M0[i-1] + rand(Poisson(dN_m₀*dt))
+        M1[i] = M1[i-1] + rand(Poisson(dN_m₁*dt))
+        M2[i] = M2[i-1] + rand(Poisson(dN_m₂*dt))
+        M3[i] = M3[i-1] + rand(Poisson(dN_m₃*dt))
+    
+        H[i] = H[i-1] + rand(Poisson(dN_h*dt))
+
+
+        # Evolucio temporal dels corrents
+        I_na = g_na * M3[i-1]/N_tot * H[i-1] * (V[i-1] - V_na)
+        I_k = g_k * N4[i-1]/N_tot * (V[i-1] - V_k)
+        I_l = g_l * (V[i-1] - V_l)
+
+        # ODE system
+        V[i] = V[i-1] + dt *  1 / C * (I_ext - I_na - I_k - I_l)
+    end
+
+#  
+end
+
+N_tot = 1e4
+dt = 1e-3
+t_tot = 1000
+
+sol = channel_states_euler(N_tot, dt, t_tot, p)
+
+myrange = 1:100:Int(round(t_tot/dt))
+fig1 = plot(sol.t[myrange],sol.V[myrange])
+fig2 = plot(sol.t[myrange],sol.N4[myrange])
+plot!(sol.t[myrange],sol.M3[myrange])
+plot!(sol.t[myrange],sol.H[myrange])
+
+plot(fig1,fig2,layout=(2,1))
+savefig("states.png")
--- gates_stochastic_euler_pol.jl
+++ gates_stochastic_euler_pol.jl
@@ -33,49 +33,49 @@ struct solution
     t::Vector{Float64}
     V::Vector{Float64}
     N_o::Matrix{Float64}
-    N_c::Matrix{Float64}
 end
 
 # Function to simulate 
 function hh_gates_simulation(N_tot, dt,t_tot, p)
     V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext = p
     total_steps = Int(t_tot/dt+1);
-
     # Inicialitzar vectors
     V = zeros(total_steps)
     N_o = zeros(Float64,3,total_steps)
-    N_c = zeros(Float64,3,total_steps)
 
     # Condicions inicials
     V[1] = rand()
     N_o[:,1] = (N_tot*rand(3))
 
     # Evolucio temporal
-    for i in 2:total_steps
+    for i in 2:Int(200/dt)
         p₁ = β(V[i-1]).*N_o[:,i-1]
         p₂ = α(V[i-1]).*(N_tot .- N_o[:,i-1])
 
-        a₁ = [rand(Poisson(p₁_i*dt)) for p₁_i in p₁];
-        a₂ = [rand(Poisson(p₂_i*dt)) for p₂_i in p₂];
+        # a₁ = [rand(Poisson(p₁_i*dt)) for p₁_i in p₁];
+        # a₂ = [rand(Poisson(p₂_i*dt)) for p₂_i in p₂];
         
+        a₁ = p₁*dt
+        a₂ = p₂*dt
+
         N_o[:,i] = N_o[:,i-1] + (a₂-a₁); 
         # N_o[:,i] = N_o[:,i-1] + dt*(-β(V[i-1]).*N_o[:,i-1] + α(V[i-1]).*(N_tot.-N_o[:,i-1]))
         # Assegurar que no hi ha estats impossibles
-        for j in 1:3
-            if N_o[j,i] < 0
-                N_o[j,i] = 0 
-                N_c[j,i] = N_tot
-            elseif N_o[j,i] > N_tot 
-                N_o[j,i] = N_tot
-                N_c[j,i] = 0
-            elseif N_c[j,i] < 0 
-                N_o[j,i] = N_tot
-                N_c[j,i] = 0
-            elseif N_c[j,i] > N_tot 
-                N_o[j,i] = 0
-                N_c[j,i] = N_tot
-            end
-        end
+        # for j in 1:3
+        #     if N_o[j,i] < 0
+        #         N_o[j,i] = 0 
+        #         N_tot .- N_c[j,i] = N_tot
+        #     elseif N_o[j,i] > N_tot 
+        #         N_o[j,i] = N_tot
+        #        N_tot .- N_c[j,i] = 0
+        #     elseif N_tot .-N_c[j,i] < 0 
+        #         N_o[j,i] = N_tot
+        #         N_c[j,i] = 0
+        #     elseif N_tot .- N_c[j,i] > N_tot 
+        #         N_o[j,i] = 0
+        #         N_tot .-N_c[j,i] = N_tot
+        #     end
+        # end
 
         # Corrents
         I_na =  g_na * (N_o[2,i-1]/N_tot)^3 * (N_o[3,i-1]/N_tot) * (V[i-1] - V_na)
@@ -86,22 +86,62 @@ function hh_gates_simulation(N_tot, dt,t_tot, p)
         V[i] = V[i-1] + dt *(1/C * (I_ext -I_na - I_k - I_l))
 
     end
-    return solution(collect(0:dt:t_tot),V,N_o,N_c)
+    I_ext = 1
+    for i in Int(200/dt):total_steps
+        p₁ = β(V[i-1]).*N_o[:,i-1]
+        p₂ = α(V[i-1]).*(N_tot .- N_o[:,i-1])
+
+        # a₁ = [rand(Poisson(p₁_i*dt)) for p₁_i in p₁];
+        # a₂ = [rand(Poisson(p₂_i*dt)) for p₂_i in p₂];
+        
+        a₁ = p₁*dt
+        a₂ = p₂*dt
+
+        N_o[:,i] = N_o[:,i-1] + (a₂-a₁); 
+        # N_o[:,i] = N_o[:,i-1] + dt*(-β(V[i-1]).*N_o[:,i-1] + α(V[i-1]).*(N_tot.-N_o[:,i-1]))
+        # Assegurar que no hi ha estats impossibles
+        # for j in 1:3
+        #     if N_o[j,i] < 0
+        #         N_o[j,i] = 0 
+        #         N_tot .- N_c[j,i] = N_tot
+        #     elseif N_o[j,i] > N_tot 
+        #         N_o[j,i] = N_tot
+        #        N_tot .- N_c[j,i] = 0
+        #     elseif N_tot .-N_c[j,i] < 0 
+        #         N_o[j,i] = N_tot
+        #         N_c[j,i] = 0
+        #     elseif N_tot .- N_c[j,i] > N_tot 
+        #         N_o[j,i] = 0
+        #         N_tot .-N_c[j,i] = N_tot
+        #     end
+        # end
+
+        # Corrents
+        I_na =  g_na * (N_o[2,i-1]/N_tot)^3 * (N_o[3,i-1]/N_tot) * (V[i-1] - V_na)
+        I_k  =  g_k * (N_o[1,i-1]/N_tot)^4 * (V[i-1] - V_k)
+        I_l  =  g_l * (V[i-1] - V_l)
+
+        # Evolucio de potencial, mitjançant euler
+        V[i] = V[i-1] + dt *(1/C * (I_ext -I_na - I_k - I_l))
+
+    end
+    return solution(collect(0:dt:t_tot),V,N_o)
 end
 
 # Parametres simulacio
 N_tot = 1e3; # Nombre total de canals
-dt = 0.5e-3;   # time steps
+dt = 0.2e-3;   # time steps
 t_tot = 1000; # temps final
+p = [V_na, V_k, V_l, g_na, g_k, g_l, C, I_ext];
 
 # Simulacio
 sol = hh_gates_simulation(N_tot, dt,t_tot, p)
 myrange = 1:10:Int(round(t_tot/dt));
 # Gràfiques
 fig1 = plot(sol.t[myrange],sol.V[myrange], label = "V"; xlabel = "t", ylabel = "V")
-fig2 = plot(sol.t[myrange],(sol.N_o[1,myrange]/N_tot).^4, label = "n⁴", xlabel = "T")
-       plot!(sol.t[myrange],(sol.N_o[2,myrange]/N_tot).^3, label = "m³")
-       plot!(sol.t[myrange],sol.N_o[3,myrange]/N_tot, label = "h")
+fig2 = plot(sol.t[myrange],(sol.N_o[1,myrange]/N_tot).^4*N_tot, label = "N", xlabel = "T")
+       plot!(sol.t[myrange],(sol.N_o[2,myrange]/N_tot).^3*N_tot, label = "M")
+       plot!(sol.t[myrange],sol.N_o[3,myrange]/N_tot*N_tot, label = "H")
 
 fig3 = plot(fig1,fig2, layout = (2,1))
-savefig(fig3,"sense_soroll")
+savefig(fig3,"gates.png")
